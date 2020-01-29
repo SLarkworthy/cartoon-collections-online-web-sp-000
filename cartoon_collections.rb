23e0833638cbc898a38069cb763afc45908080e2
Sarah Larkworthy <sarah.larkworthy@gmail.com>
2020-01-29 21:42:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,7 @@ def long_planeteer_calls(planeteer_calls)
   }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(food_array)
+  food_array.find()
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
